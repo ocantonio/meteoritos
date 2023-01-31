9d217d600fe302380b49f399edaf8681967402e4
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-01-31 09:42:07 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     bala = sprites.createProjectileFromSprite(assets.image`bala`, Nave, 200, 0)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
 })
 let meteorito: Sprite = null
 let bala: Sprite = null
@@ -7,6 +17,10 @@ let Nave: Sprite = null
 Nave = sprites.create(assets.image`Nave`, SpriteKind.Player)
 Nave.setStayInScreen(true)
 controller.moveSprite(Nave, 200, 200)
+info.setLife(3)
+info.setScore(0)
 game.onUpdateInterval(1000, function () {
     meteorito = sprites.create(assets.image`meterito`, SpriteKind.Enemy)
+    meteorito.setVelocity(-80, 0)
+    meteorito.setPosition(160, randint(0, 100))
 })
